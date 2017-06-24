317dd60b4ac8862373b2ca39f5678081886191f1
Scott C. Livingston <slivingston@cds.caltech.edu>
2017-06-24 01:42:52 -0400

    Begin to support bibtex target from panbib.py

--- panbib.py
+++ panbib.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 """panbib - map tlzoo YAML data into various formats
 """
+from __future__ import print_function
 import argparse
 import os.path
 import glob
@@ -28,12 +29,41 @@ def flatten_year_files(paths):
         entries.update(incoming)
     return entries
 
+def print_bibtex(entries):
+    monthtext = ['January', 'February', 'March', 'April', 'May',
+                 'June', 'July', 'August', 'September', 'October',
+                 'November', 'December']
+    for key, entry in entries.items():
+        if entry['type'] == 'conference paper':
+            print('@inproceedings{{{KEY},\n'
+                  '  title = {{{TITLE}}},\n'
+                  '  author = {{{AUTHORS}}},\n'
+                  '  year = {{{YEAR}}},\n'
+                  '  month = {{{MONTH}}},\n'
+                  '  booktitle = {{{BOOKTITLE}}},\n'
+                  '}}'.format(
+                      KEY=key,
+                      TITLE=entry['title'],
+                      AUTHORS=' AND '.join(entry['authors']),
+                      YEAR=entry['year'],
+                      MONTH=monthtext[entry['month']-1],
+                      BOOKTITLE=entry['booktitle']
+                  ))
+            print()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='gr1py')
     parser.add_argument('-t', '--type', metavar='TYPE', action='store',
+                        dest='out_format',
                         help='output format; support formats: bibtex')
     args = parser.parse_args()
 
     db_files = find_db()
     entries = flatten_year_files(db_files)
+
+    target_format = args.out_format.lower()
+    if target_format == 'bibtex':
+        print_bibtex(entries)
+    else:
+        pass
